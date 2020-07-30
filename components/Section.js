b17b3baae953f7c0d8c4547b9d8a03dc89cc70dc
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2020-07-30 17:11:32 +0300

    Updates

--- components/Section.js
+++
@@ -1,9 +0,0 @@
-export default class Section {
-  constructor(selectorContainer) {
-    this._container = document.querySelector(selectorContainer);
-  }
-
-  renderItem(item) {
-    this._container.append(item);
-  }
-}
