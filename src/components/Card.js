d476ea6b91689c70680a3213d4f8331419f19905
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2020-08-09 22:16:58 +0300

    refactor: add disabled for button of start testing after press

--- src/components/Card.js
+++ src/components/Card.js
@@ -77,6 +77,7 @@ export default class Card {
 
   loadingMessage() {
     this._button.textContent = "Loading data...";
+    this._button.setAttribute("disabled", true);
   }
 
   _setEventListenerForConclusion() {
